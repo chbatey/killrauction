dc606b02f67d16e24ca2257d197165b043547a99
Christopher Batey <christopher.batey@gmail.com>
2015-02-22 20:23:29 +0000

    first gatling test

---
+++ src/e2etest/scala/BasicSimulation.scala
@@ -0,0 +1,28 @@
+import info.batey.killrauction.Application
+import io.gatling.core.Predef._
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class BasicSimulation extends Simulation {
+  val app: ConfigurableApplicationContext = SpringApplication.run(classOf[Application])
+  Runtime.getRuntime.addShutdownHook(new Thread() {
+    override def run(): Unit = app.stop()
+  })
+
+  val httpConf = http
+    .baseURL("http://localhost:8080")
+    .doNotTrackHeader("1")
+
+
+  val scn = scenario("BasicSimulation")
+    .repeat(100) {
+      exec(http("request_1").post("/api/auction").basicAuth("chris", "password")).pause(10 milliseconds)
+    }
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
